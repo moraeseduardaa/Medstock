b96864197c0561b36643b773c2a2c8255790ee76
Eduarda Moraes <moraeseduarda72@gmail.com>
2026-08-28 07:42:31 -0300

    Estrutura Spring criada

---
+++ src/test/java/com/aep/medstock/MedstockApplicationTests.java
@@ -0,0 +1,15 @@
+package com.aep.medstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class MedstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
